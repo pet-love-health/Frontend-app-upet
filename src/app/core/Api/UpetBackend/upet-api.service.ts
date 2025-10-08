1819410e53728756cd0102edc30ef2d1884f112d
MarceloRenteriaUPC <149633136+MarceloRenteriaUPC@users.noreply.github.com>
2025-10-08 00:42:00 -0500

    fix: Fixed Api link.

--- src/app/core/Api/UpetBackend/upet-api.service.ts
+++ src/app/core/Api/UpetBackend/upet-api.service.ts
@@ -7,7 +7,7 @@ import { Observable, throwError } from 'rxjs';
 })
 export class UpetApiService {
 
-  private readonly baseUrl: string = 'https://web-production-4270c.up.railway.app/api/v1';
+  private readonly baseUrl: string = 'https://web-production-b53d4.up.railway.app/api/v1';
   //private readonly baseUrl: string = 'http://localhost:8000/api/v1';
 
 
